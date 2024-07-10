e313fe4a76d14aff3ff174cdd370f20d20b09cbd
uditya mandal <uditya04@gmail.com>
2024-07-10 20:19:55 +0530

    chore: Minor changes

--- README.md
+++ README.md
@@ -1,30 +1,19 @@
-# React + TypeScript + Vite
+# N-Queen Visualizer
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Deployed Link
 
-Currently, two official plugins are available:
+[https://visualizer.uditya.xyz](https://visualizer.uditya.xyz)
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Tech Stack
 
-## Expanding the ESLint configuration
+- React with Vite
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
+## Description
 
-- Configure the top-level `parserOptions` property like this:
+The N-Queen Visualizer is a tool for visually solving the N-Queen problem. The N-Queen problem involves placing N queens on an NxN chessboard such that no two queens threaten each other. This application provides an interactive way to observe the solution process step-by-step.
 
-```js
-export default {
-  // other rules...
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
-```
-
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+## Student info
+Name: [Uditya Kumar Mandal](https://uditya.xyz)
+<br>
+Registration Number: 12213711  
+Section: 9SK06
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ const Home: React.FC = () => {
       <h1 className="text-4xl font-bold mb-4">N-Queen Visualizer</h1>
       <Board board={board} />
 
-      <div className="flex flex-row gap-5 items-center">
+      <div className="flex flex-col sm:flex-row gap-5 items-center">
         <div className="mt-4 flex flex-row items-center">
           <label className="mr-2">Board Size:</label>
           <input
@@ -36,38 +36,40 @@ const Home: React.FC = () => {
             step="100"
           />
         </div>
-        {isSolving ? (
-          <>
-            {isPaused ? (
+        <div className="mt-4">
+          {isSolving ? (
+            <>
+              {isPaused ? (
+                <button
+                  className="px-4 py-2 bg-green-500 text-white mr-2 mb-2 sm:mb-0"
+                  onClick={resumeSolving}
+                >
+                  Resume
+                </button>
+              ) : (
+                <button
+                  className="px-4 py-2 bg-yellow-500 text-white mr-2 mb-2 sm:mb-0"
+                  onClick={pauseSolving}
+                >
+                  Pause
+                </button>
+              )}
               <button
-                className="mt-4 px-4 py-2 bg-green-500 text-white"
-                onClick={resumeSolving}
+                className="px-4 py-2 bg-red-500 text-white mb-2 sm:mb-0"
+                onClick={resetSolving}
               >
-                Resume
+                Reset
               </button>
-            ) : (
-              <button
-                className="mt-4 px-4 py-2 bg-yellow-500 text-white"
-                onClick={pauseSolving}
-              >
-                Pause
-              </button>
-            )}
+            </>
+          ) : (
             <button
-              className="mt-4 px-4 py-2 bg-red-500 text-white"
-              onClick={resetSolving}
+              className="px-4 py-2 bg-blue-500 text-white"
+              onClick={startSolving}
             >
-              Reset
+              Start
             </button>
-          </>
-        ) : (
-          <button
-            className="mt-4 px-4 py-2 bg-blue-500 text-white"
-            onClick={startSolving}
-          >
-            Start
-          </button>
-        )}
+          )}
+        </div>
       </div>
 
       {isSolving && !isPaused && <p className="mt-4">Solving...</p>}
